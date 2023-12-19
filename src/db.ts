e221d8d897835fa1e642a4c54c666885882c9572
Hernan Hawryluk <hernanhawryluk@gmail.com>
2023-12-19 20:41:18 -0300

    Server is working

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect("mongodb://localhost/merndb");
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import app from "./app";
+import { connectDB } from "./db";
 
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.listen(PORT);
 console.log("Server on port", process.env.PORT);
