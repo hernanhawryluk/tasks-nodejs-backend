064b6b45e1e383bcd505be26c0b47fbc7095aee8
Hernan Hawryluk <hernanhawryluk@gmail.com>
2023-12-21 12:37:09 -0300

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes";
 import tasksRoutes from "./routes/tasks.routes";
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
