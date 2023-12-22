5892a5140f3cc37c43318d64745a4104db428103
Hernan Hawryluk <hernanhawryluk@gmail.com>
2023-12-22 13:15:11 -0300

    Updated controllers for better stability

--- src/controllers/tasks.controller.ts
+++ src/controllers/tasks.controller.ts
@@ -9,44 +9,64 @@ type CustomRequest = Request & {
 };
 
 export const getTasks = async (req: CustomRequest, res: Response) => {
-  const tasks = await Task.find({
-    user: req.user.id,
-  }).populate("user");
-  res.json(tasks);
+  try {
+    const tasks = await Task.find({
+      user: req.user.id,
+    }).populate("user");
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export const createTask = async (req: CustomRequest, res: Response) => {
-  const { title, description, date, completed, important } = req.body;
+  try {
+    const { title, description, date, completed, important } = req.body;
 
-  const newTask = new Task({
-    title,
-    description,
-    date,
-    user: req.user.id,
-    completed,
-    important,
-  });
+    const newTask = new Task({
+      title,
+      description,
+      date,
+      user: req.user.id,
+      completed,
+      important,
+    });
 
-  const savedTask = await newTask.save();
-  res.json(savedTask);
+    const savedTask = await newTask.save();
+    res.json(savedTask);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export const getTask = async (req: Request, res: Response) => {
-  const task = await Task.findById(req.params.id).populate("user");
-  if (!task) return res.status(404).json({ message: "Task not found" });
-  res.json(task);
+  try {
+    const task = await Task.findById(req.params.id).populate("user");
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export const deleteTask = async (req: Request, res: Response) => {
-  const task = await Task.findByIdAndDelete(req.params.id);
-  if (!task) return res.status(404).json({ message: "Task not found" });
-  res.sendStatus(204);
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 export const updateTask = async (req: Request, res: Response) => {
-  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!task) return res.status(404).json({ message: "Task not found" });
-  res.json(task);
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
