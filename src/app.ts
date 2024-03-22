b417c18b09e75e96d94009bfa06c293c7160e3ba
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-03-22 18:48:33 -0300

    Added a 404 custom page

--- src/app.ts
+++ src/app.ts
@@ -27,9 +27,14 @@ app.use(
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("public"));
 
 app.use("/api", authRoutes);
 app.use("/api", tasksRoutes);
 app.use("/api", notesRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile("404.html", { root: __dirname + "/../views" });
+});
 
 export default app;
